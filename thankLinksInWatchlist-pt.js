b8dbc2ecd5df18de9fdda6f1006adbf32cc74d58
Chico Venancio <chicocvenancio@gmail.com>
2017-10-21 23:45:07 -0300

    ajax event listener, readd links

--- thankLinksInWatchlist-pt.js
+++ thankLinksInWatchlist-pt.js
@@ -1,140 +1,148 @@
 if (mw.config.get('wgNamespaceNumber') === -1) {
 	mw.loader.using(['ext.thanks',  'jquery.confirmable', 'oojs-ui-core', 'oojs-ui-windows']).then(function () {
-		var regex = /[?&]([^=#]+)=([^&#]*)/g,
-			diffs;
-		
 		mw.config.set( 'thanks-confirmation-required', true );
-		diffs = $('.mw-changeslist-diff');
-		diffs.each(function (i, diff) {
-			var url = diff.href,
-				match;
-			while(match = regex.exec(url)) {
-			 	diff[match[1]] = match[2];
+		function addLinks () {
+            var regex = /[?&]([^=#]+)=([^&#]*)/g,
+			diffs;
+            diffs = $('.mw-changeslist-diff');
+            diffs.each(function (i, diff) {
+                var url = diff.href,
+                    match;
+                while(match = regex.exec(url)) {
+                    diff[match[1]] = match[2];
+                }
+                if (! $(diff).attr('data-thanks-link') || $(diff).attr('data-thanks-link') === 'false') {
+                    $thankLink = $('<a class="mw-thanks-thank-link" title="Envie um agradecimento para este utilizador">agradecer</a>')
+                        .attr('href',"//"+ window.location.host + "/wiki/Special:Thanks/" + diff.diff)
+                        .attr('data-revision-id', diff.diff);
+                    $thankLink.insertAfter(diff);
+                    $(document.createTextNode(' | ')).insertAfter(diff);
+                    $(diff).attr('data-thanks-link', 'True')
+                }
+            });
+        
+            $( document ).ajaxComplete(function(e, j, a) {
+                if (a.url.startsWith('https://pt.wikipedia.org/wiki/Especial:P%C3%A1ginas_vigiadas') && j.status === 200) {
+                    addLinks();
+                }
+            });
+        }
+        addLinks();
+        mw.loader.implement("ext.thanks.thankswatchlist", function ( ) {
+            var $content = $('#content');
+            function reloadThankedState() {
+                $( 'a.mw-thanks-thank-link' ).each( function ( idx, el ) {
+                    var $thankLink = $( el );
+                    if ( mw.thanks.thanked.contains( $thankLink ) ) {
+                        $thankLink.before( mw.msg( 'thanks-thanked' ) );
+                        $thankLink.remove();
+                    }
+                } );
             }
-            if (! $(diff).attr('data-thanks-link') || $(diff).attr('data-thanks-link') === 'false') {
-                $thankLink = $('<a class="mw-thanks-thank-link" title="Envie um agradecimento para este utilizador">agradecer</a>')
-                    .attr('href',"//"+ window.location.host + "/wiki/Special:Thanks/" + diff.diff)
-                    .attr('data-revision-id', diff.diff);
-                $thankLink.insertAfter(diff);
-                $(document.createTextNode(' | ')).insertAfter(diff);
-                $(diff).attr('data-thanks-link', 'True')
+
+            // $thankLink is the element with the data-revision-id attribute
+            // $thankElement is the element to be removed on success
+            function sendThanks( $thankLink, $thankElement ) {
+                var source;
+
+                if ( $thankLink.data( 'clickDisabled' ) ) {
+                    // Prevent double clicks while we haven't received a response from API request
+                    return false;
+                }
+                $thankLink.data( 'clickDisabled', true );
+
+                if ( mw.config.get( 'wgAction' ) === 'history' ) {
+                    source = 'history';
+                } else {
+                    source = 'diff';
+                }
+
+                ( new mw.Api ).postWithToken( 'csrf', {
+                    action: 'thank',
+                    rev: $thankLink.attr( 'data-revision-id' ),
+                    source: source
+                } )
+                .then(
+                    // Success
+                    function ( data ) {
+                        var username = $thankLink.closest(
+                                source === 'history' ? 'li' : 'td'
+                            ).find( 'a.mw-userlink' ).text();
+                        // Get the user who was thanked (for gender purposes)
+                        return mw.thanks.getUserGender( username );
+                    },
+                    // Fail
+                    function ( errorCode, details ) {
+                        // If error occured, enable attempting to thank again
+                        $thankLink.data( 'clickDisabled', false );
+                        switch ( errorCode ) {
+                            case 'invalidrevision':
+                                OO.ui.alert( mw.msg( 'thanks-error-invalidrevision' ) );
+                                break;
+                            case 'ratelimited':
+                                OO.ui.alert( mw.msg( 'thanks-error-ratelimited', mw.user ) );
+                                break;
+                            default:
+                                OO.ui.alert( mw.msg( 'thanks-error-undefined', errorCode ) );
+                        }
+                    }
+                )
+                .then( function ( recipientGender ) {
+                    $thankElement.before( mw.message( 'thanks-thanked', mw.user, recipientGender ).escaped() );
+                    $thankElement.remove();
+                    mw.thanks.thanked.push( $thankLink );
+                } );
             }
-		});
-		
-		mw.loader.implement("ext.thanks.thankswatchlist", function ( ) {
-			var $content = $('#content');
-			function reloadThankedState() {
-				$( 'a.mw-thanks-thank-link' ).each( function ( idx, el ) {
-					var $thankLink = $( el );
-					if ( mw.thanks.thanked.contains( $thankLink ) ) {
-						$thankLink.before( mw.msg( 'thanks-thanked' ) );
-						$thankLink.remove();
-					}
-				} );
-			}
 
-			// $thankLink is the element with the data-revision-id attribute
-			// $thankElement is the element to be removed on success
-			function sendThanks( $thankLink, $thankElement ) {
-				var source;
+            function addActionToLinks( $content ) {
+                if ( mw.config.get( 'thanks-confirmation-required' ) ) {
+                    $content.find( 'a.mw-thanks-thank-link' ).confirmable( {
+                        i18n: {
+                            confirm: mw.msg( 'thanks-confirmation2', mw.user ),
+                            noTitle: mw.msg( 'thanks-thank-tooltip-no', mw.user ),
+                            yesTitle: mw.msg( 'thanks-thank-tooltip-yes', mw.user )
+                        },
+                        handler: function ( e ) {
+                            var $thankLink = $( this );
+                            e.preventDefault();
+                            sendThanks( $thankLink, $thankLink.closest( '.jquery-confirmable-wrapper' ) );
+                        }
+                    } );
+                } else {
+                    $content.find( 'a.mw-thanks-thank-link' ).click( function ( e ) {
+                        var $thankLink = $( this );
+                        e.preventDefault();
+                        sendThanks( $thankLink, $thankLink );
+                    } );
+                }
+            }
 
-				if ( $thankLink.data( 'clickDisabled' ) ) {
-					// Prevent double clicks while we haven't received a response from API request
-					return false;
-				}
-				$thankLink.data( 'clickDisabled', true );
+            if ( $.isReady ) {
+                // This condition is required for soft-reloads
+                // to also trigger the reloadThankedState
+                reloadThankedState();
+            } else {
+                $( reloadThankedState );
+            }
 
-				if ( mw.config.get( 'wgAction' ) === 'history' ) {
-					source = 'history';
-				} else {
-					source = 'diff';
-				}
+            $( function () {
+                addActionToLinks( $( 'body' ) );
+            } );
 
-				( new mw.Api ).postWithToken( 'csrf', {
-					action: 'thank',
-					rev: $thankLink.attr( 'data-revision-id' ),
-					source: source
-				} )
-				.then(
-					// Success
-					function ( data ) {
-						var username = $thankLink.closest(
-								source === 'history' ? 'li' : 'td'
-							).find( 'a.mw-userlink' ).text();
-						// Get the user who was thanked (for gender purposes)
-						return mw.thanks.getUserGender( username );
-					},
-					// Fail
-					function ( errorCode, details ) {
-						// If error occured, enable attempting to thank again
-						$thankLink.data( 'clickDisabled', false );
-						switch ( errorCode ) {
-							case 'invalidrevision':
-								OO.ui.alert( mw.msg( 'thanks-error-invalidrevision' ) );
-								break;
-							case 'ratelimited':
-								OO.ui.alert( mw.msg( 'thanks-error-ratelimited', mw.user ) );
-								break;
-							default:
-								OO.ui.alert( mw.msg( 'thanks-error-undefined', errorCode ) );
-						}
-					}
-				)
-				.then( function ( recipientGender ) {
-					$thankElement.before( mw.message( 'thanks-thanked', mw.user, recipientGender ).escaped() );
-					$thankElement.remove();
-					mw.thanks.thanked.push( $thankLink );
-				} );
-			}
-
-			function addActionToLinks( $content ) {
-				if ( mw.config.get( 'thanks-confirmation-required' ) ) {
-					$content.find( 'a.mw-thanks-thank-link' ).confirmable( {
-						i18n: {
-							confirm: mw.msg( 'thanks-confirmation2', mw.user ),
-							noTitle: mw.msg( 'thanks-thank-tooltip-no', mw.user ),
-							yesTitle: mw.msg( 'thanks-thank-tooltip-yes', mw.user )
-						},
-						handler: function ( e ) {
-							var $thankLink = $( this );
-							e.preventDefault();
-							sendThanks( $thankLink, $thankLink.closest( '.jquery-confirmable-wrapper' ) );
-						}
-					} );
-				} else {
-					$content.find( 'a.mw-thanks-thank-link' ).click( function ( e ) {
-						var $thankLink = $( this );
-						e.preventDefault();
-						sendThanks( $thankLink, $thankLink );
-					} );
-				}
-			}
-
-			if ( $.isReady ) {
-				// This condition is required for soft-reloads
-				// to also trigger the reloadThankedState
-				reloadThankedState();
-			} else {
-				$( reloadThankedState );
-			}
-
-			$( function () {
-				addActionToLinks( $( 'body' ) );
-			} );
-
-			mw.hook( 'wikipage.diff' ).add( function ( $content ) {
-				addActionToLinks( $content );
-			} );
-		} , {}, {
-		"cancel": "Cancelar",
-		"ok": "OK",
-		"thanks-confirmation2": "{{GENDER:$1|Enviar}} um agradecimento p\u00fablico por esta edi\u00e7\u00e3o?",
-		"thanks-error-invalidrevision": "O ID de revis\u00e3o n\u00e3o \u00e9 v\u00e1lido.",
-		"thanks-error-ratelimited": "{{GENDER:$1|Excedeu}} a sua frequ\u00eancia limite de edi\u00e7\u00f5es. Por favor, espere algum tempo e tente novamente.",
-		"thanks-error-undefined": "A a\u00e7\u00e3o de agradecimento falhou (c\u00f3digo de erro: $1). Por favor, tente novamente.",
-		"thanks-thank-tooltip-no": "{{GENDER:$1|Cancelar}} a notifica\u00e7\u00e3o de agradecimento",
-		"thanks-thank-tooltip-yes": "{{GENDER:$1|Enviar}} a notifica\u00e7\u00e3o de agradecimento",
-		"thanks-thanked": "{{GENDER:$1|{{GENDER:$2|agradecimento enviado}}}}"
-		});
-	});
+            mw.hook( 'wikipage.diff' ).add( function ( $content ) {
+                addActionToLinks( $content );
+            } );
+            } , {}, {
+                "cancel": "Cancelar",
+                "ok": "OK",
+                "thanks-confirmation2": "{{GENDER:$1|Enviar}} um agradecimento p\u00fablico por esta edi\u00e7\u00e3o?",
+                "thanks-error-invalidrevision": "O ID de revis\u00e3o n\u00e3o \u00e9 v\u00e1lido.",
+                "thanks-error-ratelimited": "{{GENDER:$1|Excedeu}} a sua frequ\u00eancia limite de edi\u00e7\u00f5es. Por favor, espere algum tempo e tente novamente.",
+                "thanks-error-undefined": "A a\u00e7\u00e3o de agradecimento falhou (c\u00f3digo de erro: $1). Por favor, tente novamente.",
+                "thanks-thank-tooltip-no": "{{GENDER:$1|Cancelar}} a notifica\u00e7\u00e3o de agradecimento",
+                "thanks-thank-tooltip-yes": "{{GENDER:$1|Enviar}} a notifica\u00e7\u00e3o de agradecimento",
+                "thanks-thanked": "{{GENDER:$1|{{GENDER:$2|agradecimento enviado}}}}"
+            }
+        );
+    });
 }
